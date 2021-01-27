4a036b0b64cc29c6fd2ade1ceeabca9e67ded0f0
mutaqim96 <mutaqim.ah@theiac.com>
2021-01-27 08:09:48 +0800

    Define API untuk Access Control

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register','AuthController@register')
+    ->name('register');
+
+Route::post('login', 'AuthController@login')
+    ->name('login');
+
+Route::group([
+    'middleware' => 'auth:api'
+], function(){
+
+    Route::get('logout','AuthContorller@logout')->name('logout');
+
+    Route::get('/permissions', 'RoleManager@permissionsIndex')
+        ->name('permissions.index')
+        ->middleware('permission:View All Permissions');
+
+        Route::get('/roles', 'RoleManager@rolesIndex')
+        ->name('roles.index')
+        ->middleware('permission:View All Roles');
+
+    Route::post('/roles/{role}/assign/{user}', 'RoleManager@rolesAddUser')
+        ->name('roles.addUser')
+        ->middleware('permission:Assign Role');
+
+    Route::post('/roles/{role}/unassign/{user}', 'RoleManager@rolesRemoveUser')
+        ->name('roles.removeUser')
+        ->middleware('permission:Unassign Role');
+});
